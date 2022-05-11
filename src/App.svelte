52ef8056e43960610b25cc5779fcbc1ba0adc11d
Steven Clontz <steven.clontz@gmail.com>
2022-05-11 22:46:14 +0000

    fix whitespace in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,71 @@
 <script lang="ts">
-	import Knowl from './Knowl.svelte';
+    import Knowl from './Knowl.svelte';
 
-	let xmlString=`
+    let xmlString=`
 <knowl mode="exercise">
-	<content>
-		<p>Some inline math <m>x^2+y^2=z^2</m>.</p>
-		<p>Some display math <m mode="display">\\sum_{k=0}^n k=\\frac{n(n+1)}{2}</m></p>
-		<p>
+    <content>
+        <p>Some inline math <m>x^2+y^2=z^2</m>.</p>
+        <p>Some display math <m mode="display">\\sum_{k=0}^n k=\\frac{n(n+1)}{2}</m></p>
+        <p>
 Hey check out <url href="https://checkit.clontz.org"><em>CheckIt</em></url>.
 It's at <url href="https://checkit.clontz.org"/>.
-		</p>
-		</content>
-	<outtro mode="answer">
-		<p>I guess an answer <em>should</em> go here.</p>
-	</outtro>
+        </p>
+        </content>
+    <outtro mode="answer">
+        <p>I guess an answer <em>should</em> go here.</p>
+    </outtro>
 </knowl>
 `.trim();
-	const parser = new DOMParser();
-	let knowlDom = parser.parseFromString(xmlString, "application/xml")
-	let knowl: Element
-	let error: boolean
-	$: if (!parser.parseFromString(xmlString, "application/xml").querySelector('parsererror')) {
-	    knowlDom = parser.parseFromString(xmlString, "application/xml")
-		knowl = knowlDom.getElementsByTagName("knowl")[0]
-		error = false
-	} else {
-		error = true
-	}
+    const parser = new DOMParser();
+    let knowlDom = parser.parseFromString(xmlString, "application/xml")
+    let knowl: Element
+    let error: boolean
+    $: if (!parser.parseFromString(xmlString, "application/xml").querySelector('parsererror')) {
+        knowlDom = parser.parseFromString(xmlString, "application/xml")
+        knowl = knowlDom.getElementsByTagName("knowl")[0]
+        error = false
+    } else {
+        error = true
+    }
 </script>
 
 <main>
-	<h1>SpaTeXt Demo</h1>
-	<div style="overflow:hidden">
-		<div class="leftBox">
-			<textarea bind:value={xmlString} class:error={error}/>
-		</div>
-		<div class="rightBox">
-			<Knowl {knowl}/>
-		</div>
-	</div>
+    <h1>SpaTeXt Demo</h1>
+    <div style="overflow:hidden">
+        <div class="leftBox">
+            <textarea bind:value={xmlString} class:error={error}/>
+        </div>
+        <div class="rightBox">
+            <Knowl {knowl}/>
+        </div>
+    </div>
 </main>
 
 <style>
-	main {
-		padding: 1em;
-		margin: 0 auto;
-	}
+    main {
+        padding: 1em;
+        margin: 0 auto;
+    }
 
-	h1 {
-		text-align: center;
-	}
+    h1 {
+        text-align: center;
+    }
 
-	@media (min-width: 640px) {
-		.leftBox {
-			width: 48%; float:left;
-		}
-		.rightBox {
-			width: 48%; float:right;
-		}
-	}
+    @media (min-width: 640px) {
+        .leftBox {
+            width: 48%; float:left;
+        }
+        .rightBox {
+            width: 48%; float:right;
+        }
+    }
 
-	textarea {
-		width:100%;
-		height:15em;
-	}
+    textarea {
+        width:100%;
+        height:15em;
+    }
 
-	.error {
-		background-color: rgb(243, 211, 211);
-	}
+    .error {
+        background-color: rgb(243, 211, 211);
+    }
 </style>
