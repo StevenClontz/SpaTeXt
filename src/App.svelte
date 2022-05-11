03c012e3f8d2623eadedcbe883f30db2d5780953
Steven Clontz <steven.clontz@gmail.com>
2022-05-11 21:07:40 +0000

    tweaks to app component

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,11 @@
 	<content>
 		<p>Some inline math <m>x^2+y^2=z^2</m>.</p>
 		<p>Some display math <m mode="display">\\sum_{k=0}^n k=\\frac{n(n+1)}{2}</m></p>
-	</content>
+		<p>
+Hey check out <url href="https://checkit.clontz.org"><em>CheckIt</em></url>.
+It's at <url href="https://checkit.clontz.org"/>.
+		</p>
+		</content>
 	<outtro>
 		<p>I guess an answer <em>should</em> go here.</p>
 	</outtro>
@@ -15,19 +19,23 @@
 	const parser = new DOMParser();
 	let knowlDom = parser.parseFromString(xmlString, "application/xml")
 	let knowl: Element
+	let error: boolean
 	$: if (!parser.parseFromString(xmlString, "application/xml").querySelector('parsererror')) {
 	    knowlDom = parser.parseFromString(xmlString, "application/xml")
 		knowl = knowlDom.getElementsByTagName("knowl")[0]
+		error = false
+	} else {
+		error = true
 	}
 </script>
 
 <main>
 	<h1>SpaTeXt Demo</h1>
 	<div style="overflow:hidden">
-		<div style="width: 48%; float:left">
-			<textarea bind:value={xmlString}/>
+		<div class="leftBox">
+			<textarea bind:value={xmlString} class:error={error}/>
 		</div>
-		<div style="width: 48%; float:right">
+		<div class="rightBox">
 			<Knowl {knowl}/>
 		</div>
 	</div>
@@ -37,7 +45,6 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
@@ -45,9 +52,20 @@
 		main {
 			max-width: none;
 		}
+		.leftBox {
+			width: 48%; float:left;
+		}
+		.rightBox {
+			width: 48%; float:right;
+		}
 	}
 
 	textarea {
-		width:100%;height:15em;
+		width:100%;
+		height:15em;
+	}
+
+	.error {
+		background-color: rgb(243, 211, 211);
 	}
 </style>
