7a4802de9a2defa75d9729b268778578aae1bae7
Steven Clontz <steven.clontz@gmail.com>
2023-03-12 15:28:50 +0000

    fix __VERSION__ declaration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import SpaTeXt from '../lib/SpaTeXt.svelte'
     let stx='<p>The Pythagorean Theorem is <me mode="inline">a^2+b^2=c^2.</me></p>\n<p>Foo.</p>'
+    const version = __VERSION__
 </script>
 
-<h1>SpaTeXt {__VERSION__}</h1>
+<h1>SpaTeXt {version}</h1>
 
 <textarea style="width:100%;height:5em" bind:value={stx}/>
 
