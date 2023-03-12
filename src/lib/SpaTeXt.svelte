d7f2f5461baf695f94d940fb36e6d0f9db61cab7
Steven Clontz <steven.clontz@gmail.com>
2023-03-12 23:12:46 +0000

    enforce contents of spatext element

--- src/lib/SpaTeXt.svelte
+++ src/lib/SpaTeXt.svelte
@@ -2,7 +2,6 @@
     import Content from './Elements/Content.svelte'
     import Division from './Elements/Division.svelte'
     import Knowl from './Elements/Knowl.svelte'
-    import P from './Elements/P.svelte'
     import Title from './Elements/Title.svelte'
     import { CheerioApi } from './stores'
     import * as Cheerio from 'cheerio'
@@ -14,18 +13,18 @@
 {#each spatext.children("title") as element}
     <h1 style="text-align:center;font-size:2.5em"><Title {element}/></h1>
 {/each}
-{#each spatext.children() as element}
-    {#if ["content"].includes(element.tagName)}
-        <Content {element}/>
-    {:else if ["knowl"].includes(element.tagName)}
-        <Knowl {element}/>
-    {:else if ["p"].includes(element.tagName)}
-        <P {element}/>
-    {:else if ["division"].includes(element.tagName)}
-        <Division {element}/>
-    {/if}
+{#each spatext.children("division") as element}
+    <Division {element}/>
 {:else}
-    <div class="warning">(No SpaTeXt elements found.)</div>
+    {#each spatext.children("content:first") as element}
+        <Content {element} allowKnowls/>
+    {:else}
+        {#each spatext.children("knowl:first") as element}
+            <Knowl {element}/>
+        {:else}
+        <div class="warning">(No SpaTeXt elements found.)</div>
+        {/each}
+    {/each}
 {/each}
 
 <style>
