ff9e0c2c39326134dde3da936e33018982f5da2b
Steven Clontz <steven.clontz@gmail.com>
2023-03-12 19:34:48 +0000

    fix reactivity

--- src/lib/Elements/Image.svelte
+++ src/lib/Elements/Image.svelte
@@ -3,12 +3,12 @@
     import { CheerioDoc } from '../stores'
     export let element:Cheerio.Element
     let src:string
-    if ($CheerioDoc(element).attr("remote")) {
+    $: if ($CheerioDoc(element).attr("remote")) {
         src = $CheerioDoc(element).attr("remote") + "/" + $CheerioDoc(element).attr("source")
     } else {
         src = "./" + $CheerioDoc(element).attr("source")
     }
-    let alt = $CheerioDoc(element).attr("description")
+    $: alt = $CheerioDoc(element).attr("description")
 </script>
 
 <img style="max-width:100%" {src} {alt}/>
