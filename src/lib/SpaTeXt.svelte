cfca8937ce818944da77680625a934de3eac3454
Steven Clontz <steven.clontz@gmail.com>
2023-03-12 22:44:32 +0000

    add title support to document

--- src/lib/SpaTeXt.svelte
+++ src/lib/SpaTeXt.svelte
@@ -3,13 +3,18 @@
     import Division from './Elements/Division.svelte'
     import Knowl from './Elements/Knowl.svelte'
     import P from './Elements/P.svelte'
+    import Title from './Elements/Title.svelte'
     import { CheerioApi } from './stores'
     import * as Cheerio from 'cheerio'
     export let stx:string
     $: CheerioApi.update(()=>Cheerio.load(stx,{xml: true}))
+    $: spatext = $CheerioApi("spatext:first")
 </script>
 
-{#each $CheerioApi("spatext:first").children() as element}
+{#each spatext.children("title") as element}
+    <h1 style="text-align:center;font-size:2.5em"><Title {element}/></h1>
+{/each}
+{#each spatext.children() as element}
     {#if ["content"].includes(element.tagName)}
         <Content {element}/>
     {:else if ["knowl"].includes(element.tagName)}
