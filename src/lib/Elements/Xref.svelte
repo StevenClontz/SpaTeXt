f2281ce4334f0717e0ce7349db9075a02634b454
Steven Clontz <steven.clontz@gmail.com>
2023-03-17 13:57:37 +0000

    mvp xrefs

---
+++ src/lib/Elements/Xref.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type * as Cheerio from 'cheerio'
+	import { createEventDispatcher } from 'svelte';
+    import { CheerioApi } from '../stores'
+    export let element:Cheerio.Element
+    let ref = $CheerioApi(element).attr("ref")
+	const dispatch = createEventDispatcher();
+	function setXref() {
+		dispatch('message');
+	}
+</script>
+
+<button on:click={setXref}>
+    {ref}
+</button>
