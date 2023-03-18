1bf611063424d921dd2bdfacbddb0082f5ccf9e7
Steven Clontz <steven.clontz@gmail.com>
2023-03-18 18:18:04 +0000

    factor out codemirror

---
+++ src/routes/CodeMirror.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import CodeMirror from "svelte-codemirror-editor"
+    import { xml } from "@codemirror/lang-xml";
+    export let value:string
+    const styles={
+        "&": {
+            "border":"1px solid #aaa",
+            "max-height":"30em"
+        }
+    }
+</script>
+
+<CodeMirror 
+    bind:value={value} 
+    lang={xml()} 
+    tabSize={4} 
+    {styles}/>
