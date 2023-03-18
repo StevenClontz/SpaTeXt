6793fdfdf9d2e1a2f096539487282f7c2d478e13
Steven Clontz <steven.clontz@gmail.com>
2023-03-18 03:14:44 +0000

    add generic parse element

--- src/lib/Elements/P.children.svelte
+++ src/lib/Elements/P.children.svelte
@@ -1,33 +1,27 @@
 <script lang="ts">
     import type * as Cheerio from 'cheerio'
     import { CheerioApi } from '../stores'
-    import C from './C.svelte'
-    import Em from './Em.svelte';
-    import Image from './Image.svelte';
-    import M from './M.svelte'
-    import Q from './Q.svelte'
-    import Url from './Url.svelte'
+    import Parse from '../Parse.svelte'
     import Xref from './Xref.svelte'
     export let element:Cheerio.Element
     export let xrefId:string|undefined = undefined
+    const allowed = [
+        "c",
+        "em",
+        "image",
+        "m",
+        "me",
+        "q",
+        "url"
+    ]
 </script>
 
 {#each $CheerioApi(element).contents() as child}
     {#if !("tagName" in child)}
         {$CheerioApi(child).text()}
-    {:else if child.tagName === "m" || child.tagName === "me"}
-        <M element={child}/>
-    {:else if child.tagName === "em"}
-        <Em element={child}/>
-    {:else if child.tagName === "c"}
-        <C element={child}/>
-    {:else if child.tagName === "q"}
-        <Q element={child}/>
-    {:else if child.tagName === "image"}
-        <Image element={child}/>
-    {:else if child.tagName === "url"}
-        <Url element={child}/>
     {:else if child.tagName === "xref"}
         <Xref element={child} bind:openXrefId={xrefId}/>
+    {:else if allowed.includes(child.tagName)}
+        <Parse element={child} {allowed}/>
     {/if}
 {/each}
