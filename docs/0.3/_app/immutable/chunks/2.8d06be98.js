bb2366a7508c92863ab92bd452175372c3bd1e44
Steven Clontz <steven.clontz@gmail.com>
2023-03-19 19:30:55 +0000

    update demo

--- docs/0.3/_app/immutable/chunks/2.8d06be98.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.97784ce3.js";export{t as component};
---
+++ docs/0.3/_app/immutable/chunks/2.aa7a03aa.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.21f2df3d.js";export{t as component};
